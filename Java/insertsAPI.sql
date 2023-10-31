4cf1e933e3da99f4ad109edb8c21ef157150594a
Luanna Di Stefani <luannastefani7@gmail.com>
2023-10-31 01:40:22 -0300

    Inserts API Java

--- Java/insertsAPI.sql
+++ Java/insertsAPI.sql
@@ -1,31 +1,15 @@
-SELECT * FROM tbEmpresa;
-SELECT * FROM tbAeroporto;
-SELECT * FROM tbColaborador;
-SELECT * FROM tbServidor;
-SELECT * FROM tbComponente;
-SELECT * FROM tbTipoComponente;
-SELECT * FROM tbUnidadeMedida;
-SELECT * FROM tbMetrica;
-select * FROM tbRegistro;
-SELECT * FROM tbProcessos;
+-- 	Corrigir para proxima sprint, criar um banco separado para os dados internos;
+INSERT INTO tbEmpresa VALUES(NULL,"92641676000182","Planeit","Planeit","Rua Haddock Lobo, 595");
 
-INSERT INTO tbEmpresa VALUES (null,'12345678900110', 'Azul Linhas Aéreas', 'Azul S.A.','');
+INSERT INTO tbColaborador VALUES(NULL,"52813382000","Andrey Rodrigues","andrey.rodriges@gmail.com","12345678","Gerente",1,NULL,"69937584884",3,NULL),
+								(NULL,"95906893024","Bruna Sanguini","bruna.sanguini@gmail.com","87654321","Analista de sistema",0,13,"83935572906",3,NULL),
+                                (NULL,"11414310099","Caetano Resende","caetano.resente@gmail.com","12345678","Desenvolvedor web",0,13,"63925544693",3,NULL),
+                                (NULL,"96501467098","Luanna Di Stefani","luanna.stefani@gmail.com","87654321","Desenvolvedt",0,13,"34922157817",3,NULL),
+                                (NULL,"98219937025","Lucas Augusto","lucas.augusto@gmail.com","12345678","Analista de infraestrutura",1,13,"28926136281",3,NULL);
 
-INSERT INTO tbAeroporto VALUE(null,'Conconhas',"Brasil",'SP','',1);
+INSERT INTO tbAeroporto VALUES(NULL,"Planeit","Brasil","São Paulo","Rua Haddock Lobo",3);
+INSERT INTO tbServidor VALUES(NULL,"ABC123","Servidor Interno","Red Het","4224513186","Servidor interno da empresa",19);
 
--- INSERT INTO tbColaborador VALUES(null,'12345678901','teste','qwd','12345678','asd',false,null,"1234444",1,1);
-INSERT INTO tbColaborador VALUES(null,'12345678902','teste','','','asd',false,null,"1234444",1,1);
-
-INSERT INTO tbServidor VALUES(NULL,'123456','Servidor 1','Linux','123467','Sistema Aerio',1);
-
-INSERT INTO tbTipoComponente VALUES(null,"CPU"),
-								   (NULL,"RAM"),
-                                   (NULL, "DISCO");
-
-INSERT INTO tbComponente VALUES (NULL,"AMD",1,23.45,1),
-								(NULL,"RAM",2,123.12,1),
-                                (NULL,"Disco",3,122.12,1);
-	
 
 INSERT INTO tbUnidadeMedida VALUES (NULL,"Gigabyte","GB");
 INSERT INTO tbUnidadeMedida VALUES (NULL," Megahertz","MHz");
@@ -34,7 +18,6 @@ INSERT INTO tbUnidadeMedida VALUES (NULL,"Inteiro","");
 INSERT INTO tbUnidadeMedida VALUES (NULL,"Graus Celsius","°C");
 INSERT INTO tbUnidadeMedida VALUES(NULL,"Número de Processos","");
 
-INSERT INTO tbMetrica VALUES(NULL,"2.80",1,2);
-INSERT INTO tbMetrica VALUES(NULL,"50",1,3);
-INSERT INTO tbMetrica VALUES(NULL,"70",1,5);
-INSERT INTO tbMetrica VALUES(NULL,"500",null,6);
+select * FROM tbColaborador;
+Select * from tbEmpresa;
+select * from tbServidor;
